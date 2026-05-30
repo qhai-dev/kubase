d6ff4a6816db35dbaef0111b7ee9910c566d97f6
qhai-dev <qhai-dev@outlook.com>
2026-05-30 16:08:00 +0800

    todo

--- frontend/app/miniApp/src/app.ts
+++ frontend/app/miniApp/src/app.ts
@@ -3,12 +3,11 @@ import { useLaunch } from "@tarojs/taro";
 
 import "./app.css";
 
-function App({ children }: PropsWithChildren<any>) {
+function App({ children }: Readonly<PropsWithChildren>) {
 	useLaunch(() => {
 		console.log("App launched.");
 	});
 
-	// children 是将要会渲染的页面
 	return children;
 }
 
--- frontend/app/webApp/README.md
+++ frontend/app/webApp/README.md
@@ -1,1 +1,1 @@
-# WebApp Toc
+# WebApp
